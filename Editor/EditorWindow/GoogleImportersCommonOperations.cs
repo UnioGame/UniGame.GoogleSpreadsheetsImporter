e8eeca866d898a3a11bee4d881576884414d9d27
Mefodei <mefodei.link@gmail.com>
2021-11-21 13:14:29 +0300

    - update spreadsheet importer ui

---
+++ Editor/EditorWindow/GoogleImportersCommonOperations.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Sirenix.OdinInspector;
+using UniModules.UniGame.GoogleSpreadsheetsImporter.Editor.SheetsImporter;
+
+namespace UniModules.UniGame.GoogleSpreadsheetsImporter.Editor.EditorWindow
+{
+    [Serializable]
+    public class GoogleImportersCommonOperations
+    {
+        private readonly GoogleSpreadsheetImporter _importer;
+
+#if ODIN_INSPECTOR
+        [ButtonGroup("Importers")]
+        [Button("Import All")]
+        [EnableIf(nameof(HasConnectedSheets))]
+#endif
+        public void Import() => _importer.Import();
+
+#if ODIN_INSPECTOR
+        [ButtonGroup("Importers")]
+        [Button("Export All")]
+        [EnableIf(nameof(HasConnectedSheets))]
+#endif
+        public void Export() => _importer.Export();
+        
+#if ODIN_INSPECTOR
+        [ButtonGroup()]
+        [EnableIf(nameof(IsValidToConnect))]
+        [Button("Connect Spreadsheets")]
+#endif
+        public void Reconnect() => _importer.Reconnect();
+        
+        public GoogleImportersCommonOperations(GoogleSpreadsheetImporter importer)
+        {
+            _importer = importer;
+        }
+        
+        
+        private bool HasConnectedSheets() =>  _importer && _importer.HasConnectedSheets;
+
+        private bool IsValidToConnect() => _importer && _importer.IsValidToConnect;
+        
+    }
+}
--- Editor/EditorWindow/GoogleSheetImporterEditorWindow.cs
+++ Editor/EditorWindow/GoogleSheetImporterEditorWindow.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-#if ODIN_INSPECTOR
+﻿#if ODIN_INSPECTOR
 
 namespace UniModules.UniGame.GoogleSpreadsheetsImporter.Editor.EditorWindow
 {
@@ -68,44 +66,5 @@ namespace UniModules.UniGame.GoogleSpreadsheetsImporter.Editor.EditorWindow
         #endregion
 
     }
-
-    [Serializable]
-    public class GoogleImportersCommonOperations
-    {
-        private readonly GoogleSpreadsheetImporter _importer;
-
-#if ODIN_INSPECTOR
-        [ButtonGroup("Importers")]
-        [Button("Import All")]
-        [EnableIf(nameof(HasConnectedSheets))]
-#endif
-        public void Import() => _importer.Import();
-
-#if ODIN_INSPECTOR
-        [ButtonGroup("Importers")]
-        [Button("Export All")]
-        [EnableIf(nameof(HasConnectedSheets))]
-#endif
-        public void Export() => _importer.Export();
-        
-#if ODIN_INSPECTOR
-        [ButtonGroup()]
-        [EnableIf(nameof(IsValidToConnect))]
-        [Button("Connect Spreadsheets")]
-#endif
-        public void Reconnect() => _importer.Reconnect();
-        
-        public GoogleImportersCommonOperations(GoogleSpreadsheetImporter importer)
-        {
-            _importer = importer;
-        }
-        
-        
-        private bool HasConnectedSheets() =>  _importer && _importer.HasConnectedSheets;
-
-        private bool IsValidToConnect() => _importer && _importer.IsValidToConnect;
-        
-    }
-    
 }
 #endif
