36505503ddaebf1a5b99860ae7273a0e97470538
Vorontcov Gleb <vorontcovg@taktikagames.ru>
2021-12-20 18:49:38 +0300

    fixed bug with ''

--- Editor/SheetsImporter/SpreadsheetData.cs
+++ Editor/SheetsImporter/SpreadsheetData.cs
@@ -14,7 +14,12 @@
             _sheets = sheets;
         }
 
-        public bool HasSheet(string sheetName) => _sheets.Any(x => x.Name.ToLower().Equals(sheetName.ToLower()));
+
+        public bool HasSheet(string sheetName)
+        {
+            // TODO: Trim only for fix the bug with 'sheet_name'
+            return _sheets.Any(x => x.Name.Trim('\'').ToLower().Equals(sheetName.ToLower()));
+        }
 
         public IEnumerable<SheetData> Sheets => _sheets;
 
