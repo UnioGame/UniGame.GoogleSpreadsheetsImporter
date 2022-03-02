977cd7eeef3e4a660f1654a4aa4ef7e9c003cd46
Mefodei <mefodei.link@gmail.com>
2022-03-02 17:56:16 +0300

    - update spreadsheet package

---
+++ Editor/SheetsImporter/Abstract/IAssetNameFormatter.cs
@@ -0,0 +1,7 @@
+namespace UniModules.UniGame.GoogleSpreadsheetsImporter.Editor.SheetsImporter.Abstract
+{
+    public interface IAssetNameFormatter
+    {
+        string FormatName(string assetName);
+    }
+}
